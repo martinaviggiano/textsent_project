1cc132342d4b38ff06a6f42dc525f34e6f48d387
martinaviggiano <martinaviggiano@users.noreply.github.com>
2021-05-03 23:42:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,43 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import time
 
 st.set_page_config(
-    page_title="Hate speech detection", page_icon="🎓", layout="centered"
+    page_title="Hate speech detection", page_icon="🔤", layout="centered"
 )
 
-st.title("Hate speech detection")
-st.write("Text mining and sentiment analysis project")
+st.header("HATE SPEECH DETECTION")
+st.subheader("Text mining and sentiment analysis project")
 st.write("Martina Viggiano (954603)")
 
-with open("data.pkl", "rb") as f:
+#@st.cache
+with open("C:/Users/marti/repos/textsent_project/data.pkl", "rb") as f:
     data = pickle.load(f)
-
+    
+st.write("This is the entire dataset")
+#@st.cache
 st.write(data)
+
+
+st.write("You can choose which label to display")
+selected_sex = st.multiselect("Select Label", data['label'].unique())
+st.write("Remember: 0 = No Hate Speech and 1 = Hate Speech")
+st.write(f"Selected Option:  {selected_sex!r}", selected_sex)
+if 0 in selected_sex:
+    st.write(data.loc[data['label'] == 0])
+if 1 in selected_sex:
+    st.write(data.loc[data['label'] == 1])
+    
+#if 0 in selected_sex and 1 in selected_sex:
+#    st.write(data)
+
+
+
+
+progress_bar = st.progress(0)
+progress_text = st.empty()
+for i in range(101):
+    time.sleep(0.1)
+    progress_bar.progress(i)
+    progress_text.text(f"Progress: {i}%")
