0c3cd999c548ec080dc62d97606449c6ea73e2e5
martinaviggiano <martinaviggiano@users.noreply.github.com>
2021-05-07 17:59:59 +0200

    Update descriptions in app.py and contractions in cleaning

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.set_page_config(
 
 
 # My custom functions
-@st.cache()
+@st.cache(allow_output_mutation=True)
 def get_data(path):
     """
     Loads the serialized objects
@@ -42,16 +42,16 @@ def get_data(path):
     Returns
     -------
     A tuple with the data pd.DataFrame, the TfidfVectorizer
-    and the SVC fitted model.
+    and the SVC fitted model.log_fit_n 
     """
     
     data = pickle.load(open(Path(path, "data.pkl"), "rb"))
     vect = pickle.load(open(Path(path, "vect.pkl"), "rb"))
     svc_i = pickle.load(open(Path(path, "svc.pkl"), "rb"))
     vect_pos = pickle.load(open(Path(path, "vect_pos.pkl"), "rb"))
-    svc_pos = pickle.load(open(Path(path, "svc_pos.pkl"), "rb"))
+    log_pos = pickle.load(open(Path(path, "log_pos.pkl"), "rb"))
 
-    return data, vect, svc_i, vect_pos, svc_pos
+    return data, vect, svc_i, vect_pos, log_pos
     
 
 # Classification functions
@@ -64,6 +64,10 @@ def expand_contractions(text):
         "PM ": "personal message ",
         " Donot ": " do not ",
         " MB ": " megabytes ",
+        "I'm" : "I am",
+        " 've " : " have ",
+        " 're " : " are ",
+        " 'll " : " will ",
     }
     
     c_re = re.compile("(%s)" % "|".join(cList.keys()))
@@ -75,30 +79,28 @@ def full_text_clean(text):
     aa = expand_contractions(text)
     
     bb = pproc.clean(
-        clean(pproc.clean(aa),
-              fix_unicode=True,               # fix various unicode errors
-              to_ascii=True,                  # transliterate to closest ASCII representation
-              lower=True,                     # lowercase text
-              no_line_breaks=True,            # fully strip line breaks as opposed to only normalizing them
-              no_urls=True,                   # replace all URLs with a special token
-              no_emails=True,                 # replace all email addresses with a special token
-              no_phone_numbers=False,         # replace all phone numbers with a special token
-              no_numbers=False,               # replace all numbers with a special token
-              no_digits=False,                # replace all digits with a special token
-              no_currency_symbols=False,      # replace all currency symbols with a special token
-              no_punct=True,                  # remove punctuations
-              replace_with_url=" ",
-              replace_with_email=" ",
-        )
+        clean(
+            pproc.clean(aa),
+            fix_unicode=True,               # fix various unicode errors
+            to_ascii=True,                  # transliterate to closest ASCII representation
+            lower=True,                     # lowercase text
+            no_line_breaks=True,            # fully strip line breaks as opposed to only normalizing them
+            no_urls=True,                   # replace all URLs with a special token
+            no_emails=True,                 # replace all email addresses with a special token
+            no_phone_numbers=False,         # replace all phone numbers with a special token
+            no_numbers=False,               # replace all numbers with a special token
+            no_digits=False,                # replace all digits with a special token
+            no_currency_symbols=False,      # replace all currency symbols with a special token
+            no_punct=True,                  # remove punctuations
+            replace_with_url=" ",
+            replace_with_email=" ")
     )
     
-    swords = stopwords.words("english")
-    swords.extend(string.punctuation)
+    swords = string.punctuation
 
     cc = (
         bb.lower()
         .replace(r"(@[a-z0-9]+)\w+", " ")
-        .replace(r"http\S+", " ")
         .replace(r"www\S+", " ")
         .replace(r"com/watch", " ")
         .replace(r"\S*[.,:;!?-]\S*[^\s\.,:;!?-]", " ")
@@ -109,9 +111,10 @@ def full_text_clean(text):
         .replace(r"ghlight", " ")
         .replace(r"[0-9]+(?:st| st|nd| nd|rd| rd|th| th)", " ")
         .replace(r"([^a-z \t])", " ")
-        .replace(r" +", " "))
+        .replace(r" +", " ")
+        .replace(r"http", " "))
     
-    cc = " ".join([i for i in cc.split() if not i in swords])
+    cc = " ".join([i for i in cc.split() if i not in swords]) ###############
     
     return cc
 
@@ -122,18 +125,38 @@ def hate_predict(X, vect, clf):
     classification = clf.predict(X_new)
     
     return classification
-       
- 
+
+
 # Pages
 def load_homepage(data):
-    or_data = data[["label" , "text"]]
-    with st.beta_expander("Show original data"):
-        st.write(or_data)
-    with st.beta_expander("Select columns to display"):
-        selected_col = st.multiselect("Select Columns", data.columns)
-        st.write(data.loc[:,selected_col])
-
-
+    st.markdown('''
+    This dashboard is used to display the preliminary analysis and the results of Sentiment Analysis module project: the goal of the study is to <strong>classify text contents as hate speech</strong> or not.
+    
+    The dataset considered in the study contains textual data extracted from <em>Stormfront</em>, a white supremacist forum.
+    
+    In particular, data were taken from the Github repository <em>hate-speech-dataset</em> by Vicomtech, in which a list of sentences from a random set of forums posts is provided.
+    
+    A few number of variables are assigned to each of the sentences, including the label of interest: for every sentence, the dataset provides the column <em>label</em> which shows whether it has been classified as hate speech (1) or not hate speech (0).
+    
+    The first step was to clean deeply textual data; we faced some difficulties working with this dataset, since it turned out to be full of contractions and _ that created some bias, so had to be removed.
+    
+    After that, we perfomed several types of analysis on the data: we derived the most common words and also with respect to some characteristics of the words themselves, such as part of speech or the fact of being used in a sentence calssified as hate speech.
+    
+    Last thing was to employ various supervised statistical methods, training them of a set of data. Among them, we selected two models that seem to better perform classification in terms of precision and f1 score.
+    In particular, they are a <strong>support vector machine</strong> model over a sample of the data balanced with respect to labels and the second one is a <strong>logistic regression</strong> over balanced data but considering only words beloning to specific parts of speech (nouns, proper nouns, verbs, adjectives, pronouns, subordinating conjunction, coordinating conjunction, "other" defined by spacy).
+    
+    ---
+    
+    In this dashboard you can find three pages: Homepage, Data Exploration, Classification.
+    
+    You are currently on the <strong>Homepage</strong>. :smile:
+    
+    On <strong>Data Exploration</strong> page you can use interactive tools to display plots and visualize data.
+    
+    On <strong>Classification</strong> page you can take advantage of the two trained model described above by writing a sentence on which you want to test the classification accuracy.
+    ''', unsafe_allow_html = True)
+    
+   
 def load_eda(data):
     # IMAGES Data Analysis
     freq_label = Image.open('images_d/Freq_labels.png')
@@ -143,33 +166,35 @@ def load_eda(data):
     top20_adjs = Image.open('images_d/Adj_tot_hate.png')
     top20_nouns = Image.open('images_d/ Noun_tot_hate.png')
     top20_propns = Image.open('images_d/Propn_tot_hate.png')
+    top20_verbs = Image.open('images_d/Verb_tot_hate.png')
 
 
     adj = Image.open('images_d/wc_a_hate.png')
     noun = Image.open('images_d/wc_n_hate.png')
     propn = Image.open('images_d/wc_p_hate.png')
+    verb = Image.open('images_d/wc_v_hate.png')
     total_cloud = Image.open('images_d/wc_tot_hate.png')
     
-    st.header("Original data")
-    st.write("""
-    You can choose which columns and label to display. Remember:
+    st.write("On this page you can use the interactive tools to explore data and gather intormation about them.") 
+    
+    st.markdown("<h3><strong>Data Table Visualization</strong></h3>", unsafe_allow_html=True)
 
-    - 0 = No Hate Speech
-    - 1 = Hate Speech
-    """)
-    selected_col = st.multiselect("Select Columns", list(data.columns.values), list(data.columns.values))
-    selected_lab = st.selectbox("Select Label", ["Hate Speech", "Non Hate Speech"])
+    st.write("Here you can build the data table by selecting columns and class of the label you want to display below.")
+    st.write("At the bottom of this page you can find the legend explaining what each column contains.")
+    
+    selected_col = st.multiselect( "Select columns:", list(data.columns.values), list(data.columns.values))
+    selected_lab = st.selectbox("Select the label:", ["Hate Speech", "Not Hate Speech"])
     selected_lab_val = 1 if selected_lab == "Hate Speech" else 0
 
     st.write(data.loc[data['label'] == selected_lab_val, selected_col])
+    
+    st.markdown("---", unsafe_allow_html=True)
 
-    st.header("Plot Section")
+    st.markdown("<h3><strong>Frequency Plots Section</h3></strong>", unsafe_allow_html=True)
 
-    st.write("In this section you can use the interactive tools to display plots of explorative analysis of data")
+    st.write("You can click on buttons below to display plots and explore data.")
 
-    st.subheader("Plot Section")
-    st.write("Select the plot to display, by clicking on one of the following buttons:")
-
+    st.write("Select the plot to display by clicking on one of the following buttons:")
 
     if st.button('Frequency per Label'):
         st.image(freq_label, caption='Frequency per Label')
@@ -189,22 +214,16 @@ def load_eda(data):
     if st.button('Top 20 Proper Nouns'):
         st.image(top20_propns, caption='Differences in frequency betweeen common Proper Nouns in Hate Speech and neutral sentences')
 
-    st.write("You can choose which label to display")
-    selected_lab = st.multiselect("Select Label", data['label'].unique())
-    st.write("Remember:")
-    st.write(" - 0 = No Hate Speech")
-    st.write(" - 1 = Hate Speech")
-    st.write(f"Selected Option:  {selected_lab!r}")
-    if 0 in selected_lab:
-        st.write(data.loc[data['label'] == 0])
-    if 1 in selected_lab:
-        st.write(data.loc[data['label'] == 1])
-        
+    if st.button('Top 20 Verbs'):
+        st.image(top20_verbs, caption='Differences in frequency betweeen common Verbs in Hate Speech and neutral sentences')
 
+    st.markdown("---", unsafe_allow_html=True)
 
-    st.subheader("Top words in WordCloud")
-    st.write("You can choose which Part of Speech to display")
-    selected_pos = st.multiselect("Select the Part of Speech", ["Adjectives", "Nouns" , "Proper Nouns", "All of them"])
+    st.markdown("<h3><strong>Top words in WordCloud</h3></strong>", unsafe_allow_html=True)
+    
+    st.write("In this section you can choose to display one or more cloud of words plots.")
+    st.write("You can choose between the cloud containing the 20 most common words for four parts of speech (Nouns, Proper Nouns, Adjectives and Verbs) or the plot that displays all of them together.")
+    selected_pos = st.multiselect("Select the Part of Speech", ["Adjectives", "Nouns" , "Proper Nouns", "Verbs", "All of them"])
     st.write(f"Selected Option:  {selected_pos!r}")
     if "Adjectives" in selected_pos:
         st.image(adj, caption='Top 20 Adjectives in Hate Speech sentences')
@@ -212,74 +231,111 @@ def load_eda(data):
         st.image(noun, caption='Top 20 Nouns in Hate Speech sentences')
     if "Proper Nouns" in selected_pos:
         st.image(propn, caption='Top 20 Proper Nouns in Hate Speech sentences')
+    if "Verbs" in selected_pos:
+        st.image(verb, caption='Top 20 Verbs in Hate Speech sentences')
     if "All of them" in selected_pos:
         st.image(total_cloud, caption='Top 20 of all the parts of speech in Hate Speech sentences')
-
-####
-def load_classif_under(data, vect, svc_i):
-    written_under = st.text_input('Write your sentence here', 'I hate nobody' , key = '1')
         
-    if not written_under:
+    st.markdown("---")
+    st.markdown("<h3><strong>Legend</h3></strong>", unsafe_allow_html=True)
+    st.markdown('''
+    - <em>label</em> = Categorical variable for which 0 is Not Hate Speech and 1 is Hate Speech;
+    - <em>text</em> = String containing the original sentence;
+    - <em>text_clean</em> = String cleaned;
+    - <em>POS_spacy</em> = List of tokens associated to the part of speech;
+    - <em>lemmatized</em> = String of tokens lemmatized by spacy;
+    - <em>tokens</em> = List of tokens retrieved by spacy;
+    - <em>language</em> = Language of the sentence defined by spacy;
+    - <em>word_count_before</em> = Number of tokens in original sentence, before cleaning;
+    - <em>word_count</em> = Number of words after cleaning;
+    - <em>word_cleaning</em> = Difference in number of words before and after cleaning;
+    - <em>NOUN</em> = List of nouns in the sentence;
+    - <em>NOUN_count</em> = Number of nouns in the sentence;
+    - <em>PROPN</em> = List of proper nouns in the sentence;
+    - <em>PROPN_count</em> = Number of proper nouns in the sentence;    
+    - <em>VERB</em> = List of verbs in the sentence;
+    - <em>VERB_count</em> = Number of verbs in the sentence;
+    - <em>ADJ</em> = List of adjectives in the sentence;
+    - <em>ADJ_count</em> = Number of advectives in the sentence;
+    - <em>ADV</em> = List of adverbs in the sentence;
+    - <em>ADV_count</em> = Number of adverbs in the sentence;
+    - <em>PRON</em> = List of pronouns in the sentence;
+    - <em>PRON_count</em> = Number of pronouns in the sentence;
+    - <em>SCONJ</em> = List of subordinating conjunction in the sentence;
+    - <em>SCONJ_count</em> = Number of subordinating conjunction in the sentence;
+    - <em>INTJ</em> = List of interjection in the sentence;
+    - <em>INTJ_count</em> = Number of interjection in the sentence;    
+    
+    ''', unsafe_allow_html=True)
+        
+
+def load_classif(data, vect, svc_i, vect_pos, log_pos):
+    st.write('''
+    On this page you can test two of the models that have been trained for the project:
+    
+    1. Support vector machine modelled on data balanced with RandomUnderSampler;
+    2. Logistic regression over data including only some parts of speech.
+    ''')
+    
+    st.write("To do so, you need to write down the sentence you want to test in the board below.")
+    
+    st.write("As you cas see, there is a sentence displayed by default. It was choosen since it clearly shows that the two models work differently: in this case, the second method performs better the classification between Hate Speech and Not Hate Speech.")
+    
+    
+    written_sent = st.text_input('Write your sentence here:', "I don't think I am racist, but I hate blacks!")
+
+    warn_lbl = st.empty()
+    if not written_sent:
         warn_lbl = st.warning('Please write the sentence you want to test')
 
-    if written_under:
-        warn_lbl = st.empty()
-        pred = hate_predict([written_under], vect, svc_i)
+    if written_sent:
+        if warn_lbl is not None:
+            warn_lbl = st.empty()
 
-        if pred == 0:
-            prediction = 'NOT HATE SPEECH'
-            color = 'green'
-        else:
-            prediction = 'HATE SPEECH'
-            color = 'red'
-     
-        st.markdown(f'The sentence has been classified as: <span style="color:{color}">**{prediction}**</span>', unsafe_allow_html=True)
+        pred_under = hate_predict([written_sent], vect, svc_i)
+        pred_pos = hate_predict([written_sent], vect_pos, log_pos)
+
+        prediction_under, color_under = get_text_color(pred_under)
+        prediction_pos, color_pos = get_text_color(pred_pos)
+
+        st.markdown("<h3><strong>Model with Undersampled data</strong></h3>", unsafe_allow_html = True)
+        st.markdown(f'The sentence has been classified as: <span style="color:{color_under}">**{prediction_under}**</span>', unsafe_allow_html=True)
+
+        st.markdown("<h3><strong>Model containing only some Parts of speech</h3></strong>", unsafe_allow_html= True)
+        st.markdown(f'The sentence has been classified as: <span style="color:{color_pos}">**{prediction_pos}**</span>', unsafe_allow_html=True)
 
 
-def load_classif_pos(data, vect_pos, svc_pos):
-    written_pos = st.text_input('Write your sentence here', 'I hate nobody' , key = '2')
-        
-    if not written_pos:
-        warn_lbl = st.warning('Please write the sentence you want to test')
-
-    if written_pos:
-        warn_lbl = st.empty()
-        pred = hate_predict([written_pos], vect_pos, svc_pos)
-
-        if pred == 0:
-            prediction = 'NOT HATE SPEECH'
-            color = 'green'
-        else:
-            prediction = 'HATE SPEECH'
-            color = 'red'
-     
-        st.markdown(f'The sentence has been classified as: <span style="color:{color}">**{prediction}**</span>', unsafe_allow_html=True)
-
+def get_text_color(pred):
+    if pred == 0:
+        prediction = 'NOT HATE SPEECH'
+        color = 'green'
+    else:
+        prediction = 'HATE SPEECH'
+        color = 'red'
+    
+    return prediction, color
 
 
 def main():
     """Main routine of the app"""
     
-    st.header("HATE SPEECH DETECTION")
-    st.subheader("Text mining and sentiment analysis project")
+    st.markdown("<h1>HATE SPEECH DETECTION</h1>", unsafe_allow_html=True)
+    st.markdown("<h4><strong>Text mining and sentiment analysis project</strong></h4>", unsafe_allow_html=True)
     st.write("Martina Viggiano (954603)")
+    st.markdown("---", unsafe_allow_html=True)
     
     app_mode = st.sidebar.radio(
         "Go to:", ["Homepage", "Data Exploration", "Classification"]
     )
     
-    data, vect, svc_i, vect_pos, svc_pos = get_data(DATA_PATH)
+    data, vect, svc_i, vect_pos, log_pos = get_data(DATA_PATH)
     
     if app_mode == "Homepage":
         load_homepage(data)
     elif app_mode == "Data Exploration":
         load_eda(data)
     elif app_mode == "Classification":
-        st.subheader("Model with Undersampled data")
-        load_classif_under(data, vect, svc_i)
-        st.subheader("Model containing only some parts of speech")
-        load_classif_pos(data, vect_pos, svc_pos)
-
+        load_classif(data, vect, svc_i, vect_pos, log_pos)
 
 if __name__ == "__main__":
     main()
---
+++ plots.py
@@ -0,0 +1,121 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+def plot_freq_labels(data, template="plotly"):
+    X = ["Non Hate Speech", "Hate Speech"]
+    Y = data["label"].value_counts().values
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Bar(
+            x=X,
+            y=Y,
+            text=Y,
+            textposition="auto",
+            marker_color=["lightblue", "royalblue"],
+            hovertemplate="Label: %{x} <br>Count: %{y}",
+        )
+    )
+
+    fig.update_layout(
+        title="Labels frequency",
+        xaxis_title="Labels",
+        yaxis_title="Counts",
+        template=template,
+    )
+
+    return fig
+
+
+def plot_word_hist(data, template="plotly"):
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Histogram(
+            x=data.word_count_before.values,
+            name="Before cleaning",
+        )
+    )
+    fig.add_trace(
+        go.Histogram(
+            x=data.word_count.values,
+            name="After cleaning",
+        )
+    )
+
+    fig.update_layout(
+        title="Words distribution",
+        xaxis_title="Number of words",
+        yaxis_title="Number of sentences",
+        barmode="stack",
+        template=template,
+    )
+
+    fig.update_xaxes(range=[0, 50])
+
+    return fig
+
+
+def plot_most_common_words(df, template="plotly"):
+    X = df.words
+    Y = df.freq
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Bar(
+            x=X,
+            y=Y,
+            hovertemplate="Word: %{x} <br>Count: %{y}",
+        )
+    )
+
+    fig.update_layout(
+        title="Top 20 most common Words in the entire dataset ",
+        xaxis_title="Word",
+        yaxis_title="Count",
+        xaxis_tickangle=-90,
+        template=template,
+    )
+
+    return fig
+
+
+def plot_top_20_pos(df, x_col="", title="", template="plotly"):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Bar(
+            x=df[x_col],
+            y=df.Freq_Tot,
+            name="Freq. tot.",
+            yaxis="y",
+            offsetgroup=1,
+            marker_color="lightblue",
+        ),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Bar(
+            x=df[x_col],
+            y=df.Freq_Hate_Speech,
+            name="Freq. Hate Speech",
+            yaxis="y2",
+            offsetgroup=2,
+            marker_color="royalblue",
+        ),
+        secondary_y=True,
+    )
+    fig.update_xaxes(title_text=x_col)
+    fig.update_yaxes(title_text="Count", secondary_y=False)
+
+    fig.update_layout(
+        title=title, template=template, yaxis2=dict(overlaying="y", side="right")
+    )
+
+    fig.update_layout(barmode="group")
+
+    return fig
